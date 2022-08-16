2ee994001c2b68bafcb708bec583173848a75952
Botshelo Tlhabanyane <79863670+BotsheloT@users.noreply.github.com>
2022-08-16 23:19:08 +0200

    first commit

---
+++ SwiftfulThinking/SwiftfulThinking/SwiftfulThinkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulThinkingApp.swift
+//  SwiftfulThinking
+//
+//  Created by Botshelo Tlhabanyane on 2022/08/16.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulThinkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
